1f1cf4b64e73f6b3bc5cda80512da97ecd64461e
John <John>
2016-08-07 18:12:08 -0400

    content imported

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,22 @@
-console.log("hi there");
+$(document).ready(function(){
+
+});
+
+//app
+var app = angular.module('jcApp',[]);
+
+//controller
+app.controller('myCtrl',['$scope','$http', function($scope,$http) {
+
+	$http.get("/data/content.json").success(function(response){
+		$scope.data = response.info;
+		$scope.firstName= $scope.data.firstname;
+    	$scope.lastName= $scope.data.lastname;
+    	$scope.about = $scope.data.about;
+    	$scope.skills = $scope.data.skills;
+    	$scope.employment = $scope.data.employment;
+    	$scope.education = $scope.data.education;
+	});
+
+
+}]);
