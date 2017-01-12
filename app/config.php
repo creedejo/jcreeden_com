092f9c97dc2e305ec43e5a7ceb0f7befe172372f
John Creeden <johncreeden@Johns-MBP.home>
2017-01-12 10:39:39 -0500

    form fix

---
+++ app/config.php
@@ -0,0 +1,10 @@
+<?php
+
+return array(
+	"DB_HOST"=>"aaa1c1k6w41dwz.cmmmzwqxiwl1.us-east-1.rds.amazonaws.com",
+	"DB_NAME"=>"jcreeden",
+	"DB_USER"=>"jcreeden",
+	"DB_PASSWORD"=>"jcreedenroot"
+	);
+
+?>
